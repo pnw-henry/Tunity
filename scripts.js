2a4b807ae9036355c15004c4c40410e37ee89055
Henry Escobar <henrye@gmail.com>
2022-07-01 18:53:30 -0700

    Created function to handle list elements and grabbed more information from API

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 const init = () => {
   const searchForm = document.getElementById("search");
   const submitButton = document.getElementById("submit");
-  const artistList = document.getElementById("artist-list");
 
   let headers = new Headers({
     "Content-Type": "application/json",
@@ -25,13 +24,34 @@ const init = () => {
           if (data.artists.length === 0) {
             const results = document.getElementById("results-heading");
             results.innerText = "Sorry, nothing found.";
-          }
-          for (const entry of data.artists) {
-            console.log(entry);
-            const li = document.createElement("li");
-            li.className = "match";
-            li.innerText = entry.name;
-            artistList.appendChild(li);
+          } else {
+            const artistArray = [];
+            for (const artist of data.artists) {
+              console.log(artist);
+              if (artist.name) {
+                artistArray.push(`Name: ${artist.name}`);
+              } else {
+                artistArray.push("Name: N/A");
+              }
+              if (artist.area) {
+                artistArray.push(`Country: ${artist.area.name}`);
+              } else {
+                artistArray.push("Country: N/A");
+              }
+              if (artist.gender) {
+                artistArray.push(`Gender: ${artist.gender}`);
+              } else {
+                artistArray.push("Gender: N/A");
+              }
+
+              if (artist.tags) {
+                artistArray.push(`Tagged as: ${artist.tags[0].name}`);
+              } else {
+                artistArray.push("No tags found");
+              }
+              createLi(artistArray);
+              artistArray.length = 0;
+            }
           }
         })
         .catch((error) => {
@@ -44,10 +64,19 @@ const init = () => {
 };
 
 function removeAll() {
-  const ul = document.getElementById("artist-list");
+  const ul = document.getElementById("artist-names");
   while (ul.firstChild) {
     ul.removeChild(ul.firstChild);
   }
+}
+
+function createLi(artistArray) {
+  const artistList = document.getElementById("artist-names");
+  const li = document.createElement("li");
+  const artistInfo = artistArray.join(" || ");
+  li.className = "artist-entry";
+  li.innerText = artistInfo;
+  artistList.appendChild(li);
 }
 
 document.addEventListener("DOMContentLoaded", init);
